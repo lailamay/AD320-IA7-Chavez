8bd18ab7beab37d25df8777155e55253c7253307
Laila Chavez <lailamay.chavez@gmail.com>
2019-03-01 20:42:46 -0800

    added code to my express files

---
+++ public/javascripts/event.js
@@ -0,0 +1,9 @@
+function changeColor() {
+    var div = window.getComputedStyle(document.getElementById('DivOne')).backgroundColor;
+
+
+if (div === 'rgb(0, 0, 255)') {
+    document.getElementById('DivOne').style.background = 'red';
+} else {
+    document.getElementById('DivOne').style.background = 'blue';
+}};
