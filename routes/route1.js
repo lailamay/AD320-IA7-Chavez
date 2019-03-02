453f8d085f9cedb043f39fd13160476981ec1d9e
Laila Chavez <lailamay.chavez@gmail.com>
2019-03-01 21:49:59 -0800

    made changes to route 2 js

--- routes/route1.js
+++ routes/route1.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-/* GET home page. */
+
+
+/* home page. */
 router.get('/', function(req, res, next) {
   res.render('route1', { title: 'Route 1' });
 });
--- routes/route2.js
+++ routes/route2.js
@@ -5,4 +5,8 @@ routes.get('/', (req, res) => {
   res.status(200).json({ message: 'You are Connected!' });
 });
 
+router.get("/:color-name", route2Callback) => {
+  res.render('route2');
+}
+
 module.exports = routes;
