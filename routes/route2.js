b9eee1a91ae98b652fd8b9bba7d896d37caf0f55
Laila Chavez <lailamay.chavez@gmail.com>
2019-03-02 00:24:38 -0800

    modified route 2 js

--- routes/route2.js
+++ routes/route2.js
@@ -1,12 +1,36 @@
-const routes = require('express').Router();
+const router = require('express').Router();
+const colors = require('../colors.json');
 
-
-routes.get('/', (req, res) => {
+router.get('/', (req, res) => {
   res.status(200).json({ message: 'You are Connected!' });
 });
 
-router.get("/:color-name", route2Callback) => {
-  res.render('route2');
-}
+router.get('/all', (req, res) => {
+  res.status(200).json(colors);
+});
 
-module.exports = routes;
+router.get('/:color', (req, res) => {
+   // res.render('route2');
+   // console.log(req.params.color);
+   // res.status(200).json(colors.colors[2]);
+   const userColor = req.params.color.toLowerCase();
+   console.log(userColor);
+   const availableColors = {
+     'black': 0,
+     'white': 1,
+     'red': 2,
+     'blue': 3,
+     'yellow': 4,
+     'green': 5
+   }
+
+   if (availableColors[userColor] || userColor === 'black') {
+     const target = availableColors[userColor]
+     res.status(200).json(colors.colors[0]);
+   } else {
+     res.status(404).json({message: "Not found"});
+   }
+});
+
+
+module.exports = router;
